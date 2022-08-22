82a7f1a14967e73e0f075140504565f9ffa5b332
Wesley Nunes <wesleynunes990@gmail.com>
2022-08-22 19:01:35 -0300

    Add delete button

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## To-do List
 
--- Criei um pequeno projeto de uma lista de tarefas utilizando HTML, CSS e Js com algumas pequenas validações no input.
-Não é permitido adcionar uma tarefa que já foi cadastrada, adcionar com o campo do input em branco ou apenas com espaços.
+-- Criei um pequeno projeto de uma lista de tarefas utilizando HTML, CSS e Js com algumas pequenas validações.
+Quando clicado na tarefa, ela aparece como concluida e muda o estilo. E clicando na lixeira ira apagar a tarefa.
+Não é permitido adicionar uma tarefa que contenha somente espaços, ou uma tarefa em branco.
 
 <img src="./Projeto_finalizado.png">
--- script.js
+++ script.js
@@ -1,36 +1,74 @@
 let txt = document.getElementById('txtarea')
 let btn_add = document.querySelector('button.add')
 let tasks = document.querySelector('div.tasks')
-let job = []
+let jobs = []
+
+
 
 function add() {
 
-    if(txt.value.trim().length == 0 || job.indexOf(txt.value) !=-1 ) {
+    if(txt.value.trim().length == 0 ) {
         alert('Tarefa invalida ou já cadastrada')
     } else {
-        job.push(txt.value)
-    
-        let txtValue = txt.value
-        let newTask = document.createElement('input')
-        newTask.type ='checkbox'
-        newTask.id ='checkTask'
-        newTask.name='checkTask'
-        
-        let taskLabel = document.createElement('label')
-        taskLabel.id ='taskLabel'
-        taskLabel.setAttribute('name','checkTask' )
-        
-        let taskDescription = document.createTextNode(txtValue[0].toUpperCase() + txtValue.substring(1) )
-        taskLabel.appendChild(taskDescription)
-        
-        let taskContainer = document.createElement('div')
-        taskContainer.classList.add('taskContainer')
-        
-        taskContainer.appendChild(newTask)
-        taskContainer.appendChild(taskLabel)
-        tasks.appendChild(taskContainer)
-           
+        addTask()
     }
-    txt.value = ''
+    
     txt.focus()
+    txt.value = ''
+   
 }
+
+function addTask() {
+
+    jobs.push(txt.value)
+
+    let txtValue = txt.value
+    let deleteItem = document.createElement('button')
+    deleteItem.id ='deleteTask'
+    deleteItem.addEventListener('click', deleteTask )
+    //cria o button
+        
+    let img = document.createElement("img");
+    img.src="./images/delete-photo-svgrepo-com.svg";
+    deleteItem.appendChild(img);
+    //cria o icon
+
+    let taskContent = document.createElement('p')
+    taskContent.id ='taskContent'
+        
+    taskContent.addEventListener('click', taskcompleted )
+    //cria o conteudo da task
+        
+    let taskDescription = document.createTextNode(txtValue[0].toUpperCase() + txtValue.substring(1) )
+    taskContent.appendChild(taskDescription)
+    //mostra o conteudo da task no paragrafo
+        
+    let taskContainer = document.createElement('div')
+    taskContainer.classList.add('taskContainer')
+        
+    taskContainer.appendChild(taskContent)
+    taskContainer.appendChild(deleteItem)
+    tasks.appendChild(taskContainer) 
+
+
+    function taskcompleted() {
+        taskContent.classList.toggle('completed')
+    }
+    
+
+    function deleteTask() {
+        taskContainer.remove()
+    } 
+
+   
+}
+    
+txt.addEventListener('keypress', function(event) {
+    const isEntKey = event.key === "Enter"
+    if(isEntKey) {
+        addTask()
+        txt.value = ''
+    }
+}) 
+
+
